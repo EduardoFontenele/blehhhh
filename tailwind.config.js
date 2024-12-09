c39664b70be05121b206e3b433ce8b156e30255a
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2024-12-09 20:17:43 -0300

    aaa

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajuste para o caminho real do seu projeto
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
